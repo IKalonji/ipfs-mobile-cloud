29a296d519158282d5902793ab7179a4d43b8839
IKalonji <IKalonji@student.wethinkcode.co.za>
2022-03-18 13:39:24 +0200

    Project submission

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# ipfs-mobile-cloud" 
+# IPFS-mobile-cloud
+
+### This is a mobile cload solution for users to store personal mobile files on IPFS
+
+
+[Video-Demo](https://youtu.be/ZmxniRNdruY)
+
+1. Get a free Tatum API Key at [Tatum](https://tatum.io/)
+2. Clone the repo
+   ```sh
+   git clone https://github.com/IKalonji/ipfs-mobile-cloud.git
+   ```
+3. cd into the project and run the below command to install the required modules
+   ```sh
+   cd mbongo-algo-wallet
+   npm install
+   ```
+4. Proceed to run the project, the navigate to you browser or build the app to run on Android
+   ```sh
+   ionic serve --open
+   ```
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+// import { create } from "ipfs-http-client"
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,65 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private loading: LoadingController,
+    private toast: ToastController) {}
+
+  slidesOptions = {
+    slidesPerView: 1
+  }
+
+  files = ['Issa.pdf', 'CV_doc.word', 'Jumpman.mp3']
+
+  upload(){
+    document.getElementById('fileupload').click()
+  }
+
+  receivedfile(file:any){
+    this.files.push(file.target.files[0].name)
+    console.log(file.target.files[0].name)
+    this.download('File uploading...', 'File successfully uploaded.')
+    this.uploadFile(file.target.files[0])
+  }
+
+  async download(loadingText: string, toastText: string){
+    let showDownload = await this.loading.create(
+      {
+        message: loadingText,
+        duration: 4000
+      }
+    );
+
+    await showDownload.present();
+
+    setTimeout(async ()=>{
+      let toast = await this.toast.create(
+        {
+          message: toastText,
+          duration: 2000
+        }
+      );
+  
+      await toast.present()
+    }, 4500)
+  }
+
+  getClient(): any {
+    // @ts-ignore
+    return create(`${environment.ipfs}:5001/api/v0`)
+  }
+
+  public async uploadFile(data: any): Promise<string> {
+    let url = ''
+    const client = this.getClient()
+
+    try {
+      const added = await client.add(data)
+      url = `${environment.ipfs}/ipfs/${added.path}`
+    } catch (error) {
+      console.log(error)
+    }
+
+    return url
+  }
 
 }
